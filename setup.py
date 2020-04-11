43a632a21bd3336f2e4cdeb31ff80a095946d7bc
Araett <martynas575@gmail.com>
2020-04-12 00:01:58 +0300

    Extract source files into module directory

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="verbatim",
+    version="0.0.1",
+    packages=['verbatim'],
+    url='',
+    license='',
+    author='Rez',
+    author_email='do@it.yourself',
+    description='Cross Discord server announcement bot.'
+)
